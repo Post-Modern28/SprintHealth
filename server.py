3d9e08efdbc520e869cdd6b4ccc3afebc6168384
rusha-mathing <rushan.is.into.math@gmail.com>
2024-11-16 17:10:49 +0300

    Перенёс код. ПРОБЛЕМА с парсингом datetime (из-за этого пока используется str) и списков

---
+++ server.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from typing import List
+from models import Sprint, History, Entity
+import csv
+
+
+app = FastAPI()
+
+
+@app.get('/get_sprint/{name}')
+def get_sprint(name: str):
+    with open('files/sprints.csv', mode='r', encoding='utf-8') as f:
+        reader = csv.DictReader(f, delimiter=';')
+        for sprint in map(Sprint.model_validate, reader):
+            print(sprint)
+            if sprint.sprint_name == name:
+                return {"result": sprint.model_dump_json()}
+    return "Not found!"
+
+
+@app.post('/save_data')
+def save_data(sprints: List[Sprint], history: List[History], entities: List[Entity]):
+    # TODO придумать как поменять формат datetime в нормальную строку.
+    with open('files/sprints.csv', mode='w', encoding='utf-8') as f:
+        writer = csv.DictWriter(f)
+        for sprint in sprints:
+            writer.writerow(sprint.model_dump())
+
+    with open('files/history.csv', mode='w', encoding='utf-8') as f:
+        writer = csv.DictWriter(f)
+        for record in history:
+            writer.writerow(record.model_dump())
+
+    with open('files/entities.csv', mode='w', encoding='utf-8') as f:
+        writer = csv.DictWriter(f)
+        for entity in entities:
+            writer.writerow(entity.model_dump())
