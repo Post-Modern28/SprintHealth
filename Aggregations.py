c70e2fc4422edc1967a8c00e2e6665795390ce96
test <jovesfun@gmail.com>
2024-11-16 16:43:33 +0300

    add: added hours count to all categories, fixed error in calculating cancelled task

--- Aggregations.py
+++ Aggregations.py
@@ -1,15 +1,15 @@
 import numpy as np
 import pandas as pd
 
-# логика: парсим файл с названиями спринтов и тасками в нём
-# делаем join на entity и сохраняем это в БД
 
-data_path = r'D:\Python codes\SprintHealth\data_for_spb_hakaton_entities\data_for_spb_hakaton_entities1-Table 1.csv'
-history_path = r'D:\Python codes\SprintHealth\data_for_spb_hakaton_entities\history-Table 1.csv'
-sprints_path = r'D:\Python codes\SprintHealth\data_for_spb_hakaton_entities\sprints-Table 1.csv'
+data_path = r'data_for_spb_hakaton_entities/data_for_spb_hakaton_entities1-Table 1.csv'
+history_path = r'data_for_spb_hakaton_entities\history-Table 1.csv'
+sprints_path = r'data_for_spb_hakaton_entities\sprints-Table 1.csv'
 
 
 def extract_sprint_names(data_path, changes_path, sprints_path):
+    # логика: парсим файл с названиями спринтов и тасками в нём
+    # делаем join на entity и сохраняем это в БД
     data = pd.read_csv(data_path, sep=';', skiprows=1)
     history = pd.read_csv(changes_path, sep=';', skiprows=1).dropna(how="all")
     sprints = pd.read_csv(sprints_path, sep=';', skiprows=1)
@@ -17,33 +17,49 @@ def extract_sprint_names(data_path, changes_path, sprints_path):
 
     sprints_normalized = sprints.explode('entity_ids').reset_index(drop=True)
     merged_df = pd.merge(sprints_normalized, data, left_on='entity_ids', right_on='entity_id', how='inner')
-    merged_df.to_csv(r'D:\Python codes\SprintHealth\data\aggregated.csv', index=False, sep=";", encoding="utf-8-sig")
-    print(merged_df.columns)
-
-# extract_sprint_names(data_path, history_path, sprints_path)
+    merged_df.to_csv(r'data\aggregated.csv', index=False, sep=";", encoding="utf-8-sig")
 
 
-def analyze_sprint(sprint_name):
-    df = pd.read_csv(r'D:\Python codes\SprintHealth\data\aggregated.csv', sep=';')
+extract_sprint_names(data_path, history_path, sprints_path)
+
+
+def analyze_sprint(sprint_name) -> dict:
+    df = pd.read_csv(r'data\aggregated.csv', sep=';')
     df = df[df['sprint_name'] == sprint_name]
 
     status_count = df['status'].value_counts().reset_index()
     status_count.columns = ['status', 'count']
     total_count = status_count['count'].sum()
-    flags = 0
-    cancelled = (df['status'].isin(['Выполнено', 'Закрыто'])) & \
-                  (df['resolution'].isin(['Отклонено', 'Отменено инициатором', 'Дубликат']))
+    cancelled = (
+            (df['status'].isin(['Выполнено', 'Закрыто']) &
+             df['resolution'].isin(['Отклонено', 'Отменено инициатором', 'Дубликат']))
+            |
+            ((df['type'] == 'Дефект') & (df['status'] == 'Отклонен исполнителем'))
+    )
+
     cancelled_count = df[cancelled].shape[0]
+    cancelled_sum = df[cancelled]['estimation'].sum()
 
     finished = (df['status'].isin(['Выполнено', 'Закрыто'])) & ~cancelled
     finished_count = df[finished].shape[0]
+    finished_sum = df[finished]['estimation'].sum()
 
     todo = df['status'] == 'Создано'
     todo_count = df[todo].shape[0]
+    todo_sum = df[todo]['estimation'].sum()
 
+    in_progress = ~(cancelled | finished | todo)
+    in_progress_count = df[in_progress].shape[0]
+    in_progress_sum = df[in_progress]['estimation'].sum()
     in_progress_count = total_count - (cancelled_count+finished_count+todo_count)
-    print(finished_count, cancelled_count, todo_count, in_progress_count)
 
-    print(status_count)
+    print(finished_count, cancelled_count, todo_count, in_progress_count)
+    response = {'todo_overload': 0, 'cancelled_overload': 0, 'backlog_overload': 0}
+    if todo_count / total_count > 0.2:
+        response['todo_overload'] = 1
+    if cancelled_count / total_count > 0.1:
+        response['cancelled_count'] = 1
+    return response
+
 
 analyze_sprint('Спринт 2024.3.1.NPP Shared Sprint')
