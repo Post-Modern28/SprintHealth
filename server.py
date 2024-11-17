28f66cdceb1853f2c0d18ae1a30e4c0be1ada853
rusha-mathing <rushan.is.into.math@gmail.com>
2024-11-17 03:33:02 +0300

    Страничка загрузки файлов.

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 from typing import List
 from models import Sprint, History, Entity
+
 import csv
+import os
 
 
 app = FastAPI()
+
 
 
 @app.get('/get_sprint/{name}')
@@ -17,19 +23,23 @@ def get_sprint(name: str):
     return "Not found!"
 
 
-@app.post('/save_data')
-def save_data(sprints: List[Sprint], history: List[History], entities: List[Entity]):
-    with open('files/sprints.csv', mode='w', encoding='utf-8') as f:
-        writer = csv.DictWriter(f)
-        for sprint in sprints:
-            writer.writerow(sprint.model_dump())
+templates = Jinja2Templates(directory="templates")
 
-    with open('files/history.csv', mode='w', encoding='utf-8') as f:
-        writer = csv.DictWriter(f)
-        for record in history:
-            writer.writerow(record.model_dump())
 
-    with open('files/entities.csv', mode='w', encoding='utf-8') as f:
-        writer = csv.DictWriter(f)
-        for entity in entities:
-            writer.writerow(entity.model_dump())
+@app.get("/", response_class=HTMLResponse)
+async def upload_page(request: Request):
+    return templates.TemplateResponse("upload.html", {"request": request})
+
+
+@app.post("/upload/")
+async def upload_files(request: Request, csv_files: List[UploadFile]):
+    if len(csv_files) != 3:
+        return {"error": "Вы можете загрузить 3 файла (sprints.csv, history.csv, entities.csv)"}
+
+    for csv_file in csv_files:
+        with open(os.path.join("files", csv_file.filename), "wb") as f:
+            f.write(await csv_file.read())
+
+    return {"message": "Файлы успешно загружены!"}
+
+
