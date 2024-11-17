3b3633fb72e23a95425066de183f4c998cb1a67f
rusha-mathing <rushan.is.into.math@gmail.com>
2024-11-17 14:53:26 +0300

    сделаль странную страшную страничку

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi import Request
 from typing import List
 from models import Sprint, History, Entity
+import Aggregations
 
 import csv
 import os
@@ -22,33 +23,26 @@ async def upload_page(request: Request):
     return templates.TemplateResponse("upload.html", {"request": request})
 
 
-@app.post("/upload/", response_class=RedirectResponse)
+@app.post("/sprints", response_class=HTMLResponse)
 async def upload_files(request: Request, csv_files: List[UploadFile]):
     if len(csv_files) != 3:
-        return {"error": "Вы можете загрузить 3 файла (sprints.csv, history.csv, entities.csv)"}
+        return {"error": "Вы можете загрузить 3 файла (sprints.csv, history.csv, data*.csv)"}
 
+    file_names = []
     for csv_file in csv_files:
         with open(os.path.join("files", csv_file.filename), "wb") as f:
+            file_names.append(os.path.join('files', csv_file.filename))
             f.write(await csv_file.read())
-    
-    # TODO: пока кривой переход к следующей страничке с передачей названия файла через url
-    return RedirectResponse(url=f"/sprints/sprints_1.csv",
-                            status_code=status.HTTP_302_FOUND)  
-
-
-@app.get('/sprints/{csv_file}', response_class=HTMLResponse)
-def get_sprints(request: Request, csv_file: str):
-    with open(os.path.join('files', csv_file), 'r') as f:
-        reader = csv.DictReader(f, delimiter=';')
-        return templates.TemplateResponse("sprints.html", 
-                                          {"request": request, 
-                                           "sprints": map(Sprint.model_validate, reader)})
+    result = Aggregations.parse_data(*sorted(file_names))  # TODO кривое отправление через сортировку
+    print(result)
+    return templates.TemplateResponse("sprints.html", 
+                                      {"request": request, 
+                                       "sprints": result['teams']['sprints']})
 
 @app.get('/sprint/{name}')
 def get_sprint_by_name(name: str):
-    with open('files/sprints.csv', mode='r', encoding='utf-8') as f:
-        reader = csv.DictReader(f, delimiter=';')
-        for sprint in map(Sprint.model_validate, reader):
-            if sprint.sprint_name == name:
-                return {"result": sprint.model_dump()}
-    return "Not found!"
+    sprint_df = Aggregations.get_sprint_by_name(name)
+    sprint_status = Aggregations.analyze_sprint(sprint_df)
+    sprint_status.update(Aggregations.parse_sprint(sprint_df))
+    sprint_name = sprint_df["sprint_name"].iloc[0] 
+    return {sprint_name: sprint_status}
